a3e51f6ac0a6fbc173740c5abcc0fd4f0940f664
EL Malhouf Mohammed Ali <monarc1937@gmail.com>
2020-04-01 13:05:17 +0200

    Use Postman API instead of json files

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const path       = require('path')
 const app        = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.static('reports'));
+
+const apikey = 'PUT YOUR API KEY HERE'
 
 class TestRunContext {
     constructor(newmanResult) {
+        this.collection       = newmanResult.collection.name;
         this.environment      = newmanResult.environment.name
         this.iterationCount   = newmanResult.run.stats.iterations.total
         this.start            = new Date(newmanResult.run.timings.started)
@@ -27,10 +31,14 @@ class TestRunContext {
     get percentagePassed() {
         return (this.testResultTotal * 100 / (this.testResultTotal + this.testResultFailed)).toFixed(2)
     }
-
-    get envFileName() {
-        return this.environment === undefined ? "No Environment file specified for the Newman Run" : this.environment
+    
+    get environmentName() {
+        return this.environment === undefined ? "No Environment specified for the Newman Run" : this.environment;
     }
+    
+    get collectionName() {
+        return this.collection === undefined ? "No Collection for the Newman Run" : this.collection;
+    } 
     
     get skippedList() {
         if(this.skipped === undefined) {
@@ -81,8 +89,8 @@ class TestRunContext {
                     "fallback": "Newman Run Summary",
                     "color": `${this.colour}`,
                     "title": "Summary Test Result",
-                    "title_link": "https://newman-app.localtunnel.me/htmlResults.html",
-                    "text": `Environment File: *${this.envFileName}*\nTotal Run Duration: *${this.runDuration}*`,
+                    "title_link": "http://newman-app.serverless.social/htmlResults.html",
+                    "text": `Collection : *${this.collectionName}* \nEnvironment : *${this.environmentName}*\nTotal Run Duration: *${this.runDuration}*`,
                     "mrkdwn": true,
                     "fields": [
                         {
@@ -131,13 +139,48 @@ class TestRunContext {
     }
 }
 
-let executeNewman = (environmentFile, iterationCount) => {
+let getCollectionUid = collectionName => {
+    const errorMessage = 'The Collection name provided is not found in your workspace. Please try again';
+    return new Promise((resolve, reject) => {
+        axios({
+            method: "GET",
+            url: `https://api.getpostman.com/collections/?apikey=${apikey}`
+        })
+        .then(response => {
+            collection = response.data.collections.find(collection => collection.name === collectionName);
+            collection != undefined ? resolve(collection.uid) : reject(errorMessage);
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+};
+
+let getEnvironmentUid = environmentName => {
+    const errorMessage = 'The Environment name provided is not found in your workspace. Please try again';
+    return new Promise((resolve, reject) => {
+        axios({
+            method: "get",
+            url:
+            `https://api.getpostman.com/environments/?apikey=${apikey}`
+        })
+        .then(response => {
+            environment = response.data.environments.find( environment => environment.name === environmentName);
+            environment != undefined ? resolve(environment.uid) : reject(errorMessage);
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+};
+
+let executeNewman = (collectionUid, environmentUid, iterationCount) => {
     return new Promise((resolve, reject) => {
         newman.run({
-            collection: './collections/Restful_Booker_Collection.json',
-            environment: environmentFile,
+            collection: `https://api.getpostman.com/collections/${collectionUid}?apikey=${apikey}`,
+            environment: `https://api.getpostman.com/environments/${environmentUid}?apikey=${apikey}`,
             iterationCount: iterationCount,
-            reporters: ['htmlextra'],
+            reporters: ['cli', 'htmlextra'],
             reporter: {
                 htmlextra: {
                     export: './reports/htmlResults.html'
@@ -152,21 +195,21 @@ let executeNewman = (environmentFile, iterationCount) => {
     })
 }
 
-function InvalidName(responseURL, message, res) {
+function InvalidArgument(responseURL, argName, message, res) {
     axios({
         method: 'post',
         url: `${responseURL}`,
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
         data: {
-            "response_type": "in_channel",
-            "attachments": [
+            'response_type': 'in_channel',
+            'attachments': [
                 {
-                    "color": "danger",
-                    "title": "Invalid Environment",
-                    "mrkdwn": true,
-                    "fields": [
+                    'color': 'danger',
+                    'title': `Invalid ${argName}`,
+                    'mrkdwn': true,
+                    'fields': [
                         {
-                            "value": `${message}`
+                            'value': `${message}`
                         }
                     ]
                 }
@@ -182,81 +225,86 @@ app.post("/newmanRun", (req, res) => {
     const responseURL = req.body.response_url
     const channelText = req.body.text
 
-    const enteredEnv     = (channelText).split(" ")[0]
-    const iterationCount = parseInt((channelText).split(" ")[1])
-    
-    const filename = `./environments/${enteredEnv}_Restful_Booker_Environment.json`
-    
-    const fileNameCheck = fs.existsSync(filename)
-
+    const collectionName = channelText.split(" & ")[0];
+    const environmentName = channelText.split(" & ")[1];
+    const iterationCount = parseInt(channelText.split(" & ")[2]);
+        
     if (channelText.length === 0) {
-        
-        message = "Please enter an valid *Environment* name."
-        
-        return InvalidName(responseURL, message, res)
-
-    } else if (fileNameCheck === false) {
-        
-        message = `Could not find the *${path.basename(filename)}* environment file. Please try again.` 
-        
-        return InvalidName(responseURL, message, res)
-
-    } else {
-        environmentFile = filename
+        message = "Please enter a valid *Command* .";
+        return InvalidArgument(responseURL,'Slack command', message, res);
+    } else if (collectionName === undefined || environmentName === undefined) {
+        message = `Could not find the Collection or the Environment in your command. Please try again.`;
+        return InvalidArgument(responseURL, 'Slack command', message, res);
     }
 
-    axios({
-        method: 'post',
-        url: `${responseURL}`,
-        headers: { "Content-Type": "application/json" },
-        data: { 
-            "response_type": "in_channel",
-            "attachments": [
-                {
-                    "color": "good",
-                    "title": "Newman Test Run Started",
-                    "mrkdwn": true,
-                    "fields": [
+    getCollectionUid(collectionName)
+    .then(collectionUid => {
+        getEnvironmentUid(environmentName)
+        .then(environmentUid => {
+            axios({
+                'method': "post",
+                'url': `${responseURL}`,
+                'headers': { "Content-Type": "application/json" },
+                'data': {
+                    'response_type': "in_channel",
+                    'attachments': [
                         {
-                            "value": `Your Summary Report for the *${enteredEnv}* environment will be with you _very_ soon`
+                            'color': "good",
+                            'title': "Newman Test Run Started",
+                            'mrkdwn': true,
+                            'fields': [
+                                {
+                                    'value': `Your Summary Report for the collection *${collectionName}* in *${environmentName}* environment will be with you _very_ soon`
+                                }
+                            ]
                         }
                     ]
                 }
-            ]
-        }
-    })
-    .then(res.status(202).end())
-    .then(() => executeNewman(environmentFile, iterationCount))
-    .then(newmanResult => { return new TestRunContext(newmanResult) })
-    .then(context => {
-        return axios({
-            method: 'post',
-            url: `${responseURL}`,
-            headers: { "Content-Type": "application/json" },
-            data: context.slackData
-        })
-    })
-    .catch(err => {
-        axios({
-            method: 'post',
-            url: `${responseURL}`,
-            headers: { "Content-Type": "application/json" },
-            data: { 
-                "response_type": "in_channel",
-                "attachments": [
-                    {
-                        "color": "danger",
-                        "title": "Newman Run Error",
-                        "fields": [
-                            {
-                                "value": `${err}`
+            })
+            .then(res.status(202).end())
+            .then(() => {
+                    executeNewman(collectionUid, environmentUid, iterationCount)
+                    .then(newmanResult => {
+                        return new TestRunContext(newmanResult);
+                    })
+                    .then(context => {
+                        return axios({
+                            'method': "post",
+                            'url': `${responseURL}`,
+                            'headers': { "Content-Type": "application/json" },
+                            'data': context.slackData
+                        });
+                    })
+                    .catch(err => {
+                        axios({
+                            method: "post",
+                            url: `${responseURL}`,
+                            headers: { "Content-Type": "application/json" },
+                            data: {
+                                response_type: "in_channel",
+                                attachments: [
+                                    {
+                                        color: "danger",
+                                        title: "Newman Run Error",
+                                        fields: [
+                                            {
+                                                value: `${err}`
+                                            }
+                                        ]
+                                    }
+                                ]
                             }
-                        ]
-                    }
-                ]
-            }
-        })
+                        });
+                    });
+            });
+         })
+        .catch(error => {
+            InvalidArgument(responseURL, "Environment", error, res);
+        });
     })
+    .catch(error => {
+        InvalidArgument(responseURL, "Collection", error, res);
+    });
 })
 const port = Number(process.env.PORT || 3000)
 app.listen(port)
